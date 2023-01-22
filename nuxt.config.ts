506ee780c26f511e1ca43356829a9f1690369686
David Tabachnikov <nethunter@studiosh.com>
2023-01-22 10:27:22 +0100

    Fix drawing images

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   runtimeConfig: {
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    REPLICATE_API: process.env.REPLICATE_API,
   },
   modules: ["@nuxtjs/tailwindcss", "nuxt-icon", "@vueuse/nuxt"],
 });
